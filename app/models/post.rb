7409b74c020ae972e9d95f6f4fc5afb0db519873
Thilak <thilak.sankar27@gmail.com>
2021-02-16 11:32:04 +0530

    Added test

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ApplicationRecord
   belongs_to :topic
+  validates :content, presence: true, length: { minimum:10 }
 end
--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Topic, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @topic = Topic.create!(title: "First Project", post: "Any stuffs related to title")
+  end
+  describe "creation " do
+    it "should have a post" do
+      expect(Topic.all.count).to eq(1)
+    end
+  end
 end
