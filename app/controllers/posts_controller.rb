0a187e139aa9cacf7cb6bc378fb2f0d0a3b64018
Thilak <thilak.sankar27@gmail.com>
2021-02-16 13:25:34 +0530

    made some changes and addded test

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,17 @@
 class PostsController < ApplicationController
+  def new
+    @post = Post.new
+  end
+
   def create
     @topic = Topic.find(params[:topic_id])
     @post = @topic.posts.create(post_params)
+    if @post.save
     redirect_to topic_path(@topic)
+    else
+      flash[:notice] = "Must have atleast 10 character"
+      redirect_to topic_path(@topic)
+    end
   end
   def destroy
     @topic = Topic.find(params[:topic_id])
